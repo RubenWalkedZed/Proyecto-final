5e0764d95de00276459da6d8c61bfc9d22ad4256
RubenWalkedZed <79928385+RubenWalkedZed@users.noreply.github.com>
2022-03-17 20:26:30 -0400

    Add files via upload

---
+++ cuentas.js
@@ -0,0 +1,16 @@
+function getCuentas() {
+    return [
+        { name: "Transporte", type: "outcome" },
+        { name: "Supermercado", type: "outcome" },
+        { name: "Tarjeta credito", type: "outcome" },
+        { name: "Gasolina", type: "outcome" },
+        { name: "Comunicación", type: "outcome" },
+        { name: "Pago nomina", type: "income" },
+        { name: "Lotto", type: "income" },
+        { name: "Otros", type: "income" },
+        { name: "Tarjeta de debito", type: "income" },
+        { name: "Transporte", type: "income" },
+        { name: "Galon gasolina", type: "income" },
+        { name: "Entretenimiento", type: "income" },
+    ]
+}
---
+++ listado.js
@@ -0,0 +1,39 @@
+function formatearFecha(fecha) {
+    const meses = [
+        "Ene",
+        "Feb",
+        "Mar",
+        "Abr",
+        "May",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dic"
+    ];
+    return `${fecha.getDate()} / ${meses[fecha.getMonth()]}`
+}
+
+window.addEventListener('load', function(event) {
+    const data = getTransacciones();
+    const listado = document.querySelector("#listado");
+
+    for(let i=0; i < data.length; i++) {
+        const tran = data[i];
+        const fechaRegistro = formatearFecha(new Date(tran.fecha_registro));
+        listado.innerHTML = listado.innerHTML +
+        `
+        <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">  
+            <h5 class="mb-1">${tran.cuenta.name}</h5>
+            <small class="text-muted">${fechaRegistro}</small>
+            </div>
+            <p class="mb-1">${tran.notas}</p>
+            <small class="text-muted">And some muted small print.</small>
+        </a>
+        `;
+    }
+})
+
